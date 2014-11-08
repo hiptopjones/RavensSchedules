ac0ba6d07d0b7a11008cbbb4e63c273b4efc056f
hiptopjones <github@flinch.ca>
2014-11-07 23:49:35 -0800

    Generate schedules for the rest of this year.

---
+++ Schedules/GenerateSchedules.bat
@@ -0,0 +1,14 @@
+﻿
+setlocal
+
+set TOOL_PATH=V:\other\hobby\windows\IceScheduler\ScheduleTool\bin\Debug
+set MASTER_INPUT_PATH=V:\other\Ravens\2014_2015\IceAllocation\Masters\Master.csv
+set WEEKLY_OUTPUT_DIR=V:\other\Ravens\2014_2015\IceAllocation\Schedules\Schedules
+
+%TOOL_PATH%\ScheduleTool.exe -r flat -i "%MASTER_INPUT_PATH%" -w flat -o "%WEEKLY_OUTPUT_DIR%\RavensWeeklySchedule-20141110.csv" -p rebase -a 11/10/14
+%TOOL_PATH%\ScheduleTool.exe -r flat -i "%MASTER_INPUT_PATH%" -w flat -o "%WEEKLY_OUTPUT_DIR%\RavensWeeklySchedule-20141117.csv" -p rebase -a 11/17/14
+%TOOL_PATH%\ScheduleTool.exe -r flat -i "%MASTER_INPUT_PATH%" -w flat -o "%WEEKLY_OUTPUT_DIR%\RavensWeeklySchedule-20141124.csv" -p rebase -a 11/24/14
+%TOOL_PATH%\ScheduleTool.exe -r flat -i "%MASTER_INPUT_PATH%" -w flat -o "%WEEKLY_OUTPUT_DIR%\RavensWeeklySchedule-20141201.csv" -p rebase -a 12/1/14
+%TOOL_PATH%\ScheduleTool.exe -r flat -i "%MASTER_INPUT_PATH%" -w flat -o "%WEEKLY_OUTPUT_DIR%\RavensWeeklySchedule-20141208.csv" -p rebase -a 12/8/14
+%TOOL_PATH%\ScheduleTool.exe -r flat -i "%MASTER_INPUT_PATH%" -w flat -o "%WEEKLY_OUTPUT_DIR%\RavensWeeklySchedule-20141215.csv" -p rebase -a 12/15/14
+
