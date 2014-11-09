cecbe1619a22197f066c7336edacf5082e0037fe
hiptopjones <github@flinch.ca>
2014-11-08 22:41:13 -0800

    Added games to the weekly schedule, and moved Midget C1/C2 to early morning slots to make up for lost Oval slots.

--- Scripts/SplitGameSchedule.bat
+++ Scripts/SplitGameSchedule.bat
@@ -5,7 +5,7 @@ set TOOL_PATH=V:\other\hobby\windows\IceScheduler\ScheduleTool\bin\Debug
 set GAMES_DIR=V:\other\Ravens\2014_2015\Schedules\Games
 set RAVENS_GAMES_PATH=%GAMES_DIR%\RavensLeagueGames-November2014.csv
 
-%TOOL_PATH%\ScheduleTool.exe -r pcaha -i "%GAMES_DIR%\2014-2015 Female Regular Season Schedule Preview.csv" -w flat -o "%RAVENS_GAMES_PATH%" -p filter -a richmond
+%TOOL_PATH%\ScheduleTool.exe -r pcaha -i "%GAMES_DIR%\2014-2015 Female Regular Season Schedule - Final.csv" -w flat -o "%RAVENS_GAMES_PATH%" -p filter -a richmond
 %TOOL_PATH%\ScheduleTool.exe -r flat -i "%RAVENS_GAMES_PATH%" -w flat -o "%GAMES_DIR%\RavensGamesSchedule-20141110.csv" -p filter -a week46-2014
 %TOOL_PATH%\ScheduleTool.exe -r flat -i "%RAVENS_GAMES_PATH%" -w flat -o "%GAMES_DIR%\RavensGamesSchedule-20141117.csv" -p filter -a week47-2014
 %TOOL_PATH%\ScheduleTool.exe -r flat -i "%RAVENS_GAMES_PATH%" -w flat -o "%GAMES_DIR%\RavensGamesSchedule-20141124.csv" -p filter -a week48-2014
