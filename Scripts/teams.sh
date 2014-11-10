fccec56f3307f35810687cf69508480f868b9262
hiptopjones <github@flinch.ca>
2014-11-09 22:39:24 -0800

    Add script used to open up each team's schedule web page.

---
+++ Scripts/teams.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379243-Tyke-C1/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379244-Tyke-C2/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379245-Novice-C1/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379246-Novice-C2/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379248-Atom-C1/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379249-Atom-C2/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379250-Atom-C3/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379251-Peewee-A1/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379252-Peewee-A2/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379253-Peewee-C1/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379254-Bantam-A/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379255-Bantam-C1/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379256-Bantam-C2/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379257-Midget-A/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379258-Midget-C1/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379259-Midget-C2/events/new?scheduler=multiple
+open -a /Applications/Google\ Chrome.app http://www.richmondravenshockey.ca/teams/379260-Juvenile/events/new?scheduler=multiple
